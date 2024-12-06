a9fecb048554382784dabac4bf0d588ea52c8732
danger-3003 <ytprogamer1213@gmail.com>
2024-12-06 18:59:43 +0530

    updated Upcoming Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Home from "./Pages/Home"
 import About from "./Pages/About"
+import Ongoing from "./Pages/Ongoing"
 import Projects from "./Pages/Projects"
-import OtherProjects from "./Pages/OtherProjects"
+import OtherProjects from "./Pages/Other"
 import Contact from "./Pages/Contact"
 
 function App() {
   return (
     <>
         <Home />
+        <Ongoing />
         <Projects />
         <OtherProjects />
         <About />

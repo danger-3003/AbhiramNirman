b7b96d2216372ecaedaa71c6353a0d3ae4523a54
danger-3003 <ytprogamer1213@gmail.com>
2024-12-07 12:42:18 +0530

    updated animations and dimensions

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -11,10 +11,10 @@ function Projects() {
     };
     return (
         <div
-            className="h-screen w-full pt-14"
+            className="h-[28rem] sm:h-[32rem] md:h-[35rem] w-full pt-14"
             style={{
                 background: `linear-gradient(to bottom,rgba(0,0,0,0.8),rgba(0,0,0,0.2),rgba(0,0,0,0),rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url(${Touch})`,
-                backgroundPosition: "center top",
+                backgroundPosition: "right top",
                 backgroundSize: "cover",
             }}
         >
@@ -50,44 +50,49 @@ function Projects() {
                     </div>
                 </div>
                 <div
-                    className={`w-full text-white flex items-center justify-center flex-col`}
+                    className={`w-full text-white flex items-center justify-center flex-col py-5 px-3 sm:px-5 absolute bottom-0 rounded-t-3xl transition-all duration-500 
+                        ${
+                            visible
+                                ? "bg-gradient-to-b from-[rgba(0,0,0,0.4)] via-[rgba(0,0,0,0.1)] to-[rgba(0,0,0,0.4))] backdrop-blur-sm"
+                                : "bg-gradient-to-b from-[rgba(0,0,0,0.0)] via-[rgba(0,0,0,0.0)] to-[rgba(0,0,0,0.0))] backdrop-blur-none"
+                        }
+                    `}
                 >
                     <div
-                        className={`w-full flex items-center justify-center flex-col py-5 px-3 sm:px-5 absolute rounded-t-3xl transition-all duration-500 
-                            ${
-                            visible
-                                ? "bottom-0 bg-gradient-to-b from-[rgba(0,0,0,0.4)] via-[rgba(0,0,0,0.1)] to-[rgba(0,0,0,0.4))] backdrop-blur-sm"
-                                : "-bottom-[9rem] md:-bottom-[10rem] lg:-bottom-[8rem] bg-gradient-to-b from-[rgba(0,0,0,0.0)] via-[rgba(0,0,0,0.0)] to-[rgba(0,0,0,0.0))] backdrop-blur-none"
-                            }
-                        `}
+                        className="flex items-center justify-center flex-col hover:cursor-pointer mb-2"
+                        onClick={handleVisible}
                     >
-                        <div
-                            className="flex items-center justify-center flex-col hover:cursor-pointer"
-                            onClick={handleVisible}
-                        >
-                            <FontAwesomeIcon
-                                icon={faChevronUp}
-                                className={`${visible?"rotate-180":"rotate-0"} transition-all duration-500 text-lg`}
-                            />
-                            <p className="text-2xl font-bold">Condominium</p>
-                            <p className="text-xl font-light">
-                                G+15 Floors | ~2145 SFT
-                            </p>
-                        </div>
-                        <p
-                            className={` text-sm md:text-base lg:text-lg xl:text-xl sm:w-[35rem] md:w-[40rem] lg:w-[50rem] text-center xl:w-[65rem] mt-10`}
-                        >
-                            Abhiram’s Touchstone Towers offers 2 & 3 BHK
-                            Vasthu-compliant apartments (1085–2145 sq. ft.) in a
-                            prime location near Kurmannapalem Junction. These
-                            15-floor towers feature no common walls, cellar
-                            parking, a multi-level clubhouse, 24/7 security,
-                            power backup, and landscaped greenery, ensuring
-                            comfort, ventilation, and a peaceful lifestyle.
+                        <FontAwesomeIcon
+                            icon={faChevronUp}
+                            className={`${
+                                visible ? "rotate-180" : "rotate-0"
+                            } transition-all duration-500 text-base md:text-lg`}
+                        />
+                        <p className="text-xl lg:text-2xl font-bold">
+                            Condominium
+                        </p>
+                        <p className="text-lg lg:text-xl font-light">
+                            G+15 Floors | ~2145 SFT
                         </p>
                     </div>
+                    <p
+                        className={`${
+                            visible
+                                ? "relative bottom-0"
+                                : "absolute -bottom-96"
+                        } transition-all duration-500 text-sm md:text-base lg:text-lg xl:text-xl sm:w-[35rem] md:w-[40rem] lg:w-[50rem] text-center xl:w-[65rem] pt-5 lg:pt-10`}
+                    >
+                        Abhiram’s Touchstone Towers offers 2 & 3 BHK
+                        Vasthu-compliant apartments (1085–2145 sq. ft.) in a
+                        prime location near Kurmannapalem Junction. These
+                        15-floor towers feature no common walls, cellar parking,
+                        a multi-level clubhouse, 24/7 security, power backup,
+                        and landscaped greenery, ensuring comfort, ventilation,
+                        and a peaceful lifestyle.
+                    </p>
                 </div>
             </div>
+            <div></div>
         </div>
     );
 }

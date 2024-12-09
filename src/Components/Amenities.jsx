9e75f987d49602fbf2a3fe2d2d40d2f1bb6debc9
danger-3003 <ytprogamer1213@gmail.com>
2024-12-09 11:53:15 +0530

    Updated About Section

--- src/Components/Amenities.jsx
+++ src/Components/Amenities.jsx
@@ -9,7 +9,7 @@ import TableTennis from "../assets/Amenities/Touchstone/tableTennis.jpg"
 function Amenities() {
   return (
     <div>
-      <Marquee gradient gradientColor='white' gradientWidth={25} pauseOnHover autoFill className='text-white'>
+      <Marquee gradient gradientColor='#e8f7ff' gradientWidth={25} pauseOnHover autoFill className='text-white'>
         <div className='bg-white rounded-lg h-28 md:h-36 lg:h-40 group w-max flex items-center justify-center relative flex-col mx-3 my-2' style={{boxShadow:"0px 0px 10px #00000038"}}>
             <img src={Cafeteria} alt="Cafeteria" className='h-28 md:h-36 lg:h-40 rounded-lg object-cover'/>
             <div className='absolute bottom-0 bg-gradient-to-b from-[#00000000] to-[#000000] w-full py-2 h-10 group-hover:h-full transition-all duration-300 flex items-center justify-center rounded-b-lg'>
